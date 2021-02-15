79155758536daab656ca2db7b6379eedaf20b1df
Lizzie <lchan217@gmail.com>
2021-02-15 16:17:50 -0700

    add routes and test fiels

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'authors#home'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :authors, only: [:index, :show, :create, :update, :destroy]
+      resources :books, only: [:index, :show, :create, :update, :destroy]
+      resources :references, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
 end
