0810b19d9d5ad83cbbc6bbeca87a41041b3caee6
Lizzie <lchan217@gmail.com>
2021-02-20 11:27:20 -0700

    add delete functionality

--- app/javascript/packs/components/References/Reference.jsx
+++ app/javascript/packs/components/References/Reference.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import ReferenceBook from './ReferenceBook'
 
 const Reference = (props) => {
-  const { name } = props.reference
+  const { name, id } = props.reference
+
+  function deleteReference(id) {
+    fetch(`http://localhost:3000/api/v1/references/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+    })
+  }
 
   return (
     <div>
@@ -11,6 +21,7 @@ const Reference = (props) => {
           <h5 className="card-title">{name}</h5>
           <div className="card-text">
             <ReferenceBook books={props.reference.books} />
+            <button onClick={() => deleteReference(id)}>Delete Reference</button>
           </div>
         </div>
       </div>
