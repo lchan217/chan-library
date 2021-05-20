c27552a65eb182788586293d4dd14559eca99815
Lizzie <lchan217@gmail.com>
2021-05-20 12:11:31 -0600

    book & author containers to functional components

--- app/javascript/packs/components/Containers/AuthorContainer.jsx
+++ app/javascript/packs/components/Containers/AuthorContainer.jsx
@@ -1,69 +1,61 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import AuthorIndex from '../Authors/AuthorIndex'
 import AuthorShow from '../Authors/AuthorShow'
 
-class AuthorContainer extends Component {
-  state = {
-    authors: [],
-    authorShowPage: false,
-    author: []
-  }
+const AuthorContainer = () => {
+  const [ authors, setAuthors ] = useState([])
+  const [ authorShowPage, setAuthorShowPage ] = useState(false)
+  const [ author, setAuthor ] = useState([])
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("http://localhost:3000/api/v1/authors")
       .then(response => response.json())
-      .then(response => {
-        this.setState({
-            authors: response
-        })
+      .then(responseData => {
+        setAuthors(responseData)
       })
       .catch(error => console.log(error))
-  }
+  }, [])
 
-  handleClick = (event) => {
+  const handleClick = (event) => {
     const id = event.target.id
     
     fetch(`http://localhost:3000/api/v1/authors/${id}`)
     .then(response => response.json())
     .then(response => {
-      this.setState({
-        author: response,
-        authorShowPage: true
-      })
+      setAuthor(response)
+      setAuthorShowPage(true)
     })
     .catch(error => console.log(error))
   }
 
-  goBack = () => {
-    this.setState({ authorShowPage: false })
+  const goBack = () => {
+    setAuthorShowPage(false)
   }
+  
+  let authorData = null
 
-  render() {
-    let authorData = null
-
-    if(this.state.authorShowPage){
-      authorData = (
-        <div>
-          <button onClick={this.goBack}>Go Back</button>
-          <AuthorShow author={this.state.author}/>
-        </div>
-      )
-    } else {
-      authorData = (
-        <div onClick={this.handleClick}>
-          <AuthorIndex authors={this.state.authors} />
-        </div>
-      )
-    }
-
-    return (
-      <div >
-        <h1>Authors</h1>
-        <h6>Pick a row to see the author and their Books</h6>
-        {authorData}
+  if(authorShowPage){
+    authorData = (
+      <div>
+        <button onClick={goBack}>Go Back</button>
+        <AuthorShow author={author}/>
       </div>
-    );
+    )
+  } else {
+    authorData = (
+      <div onClick={handleClick}>
+        <AuthorIndex authors={authors} />
+      </div>
+    )
   }
+
+  return (
+    <div >
+      <h1>Authors</h1>
+      <h6>Pick a row to see the author and their Books</h6>
+      {authorData}
+    </div>
+  );
 }
 
 export default AuthorContainer;
